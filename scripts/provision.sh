84d38bba992c224fba1218019c480359f378d492
DefSecSentinel <48036388+DefSecSentinel@users.noreply.github.com>
2020-08-25 09:07:34 -0500

    Update provision.sh
    Updated provision script for ELK deployment

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,98 +2,156 @@
 
 #UpdateRepo
 
-sudo apt-get update
+sudo yum update -y
 
-#Depackageandinstallsplunk
+sudo yum update --security -y
 
-# Check if Splunk is already installed
-if [ -f "/opt/splunk/bin/splunk" ]; then
-    echo "[$(date +%H:%M:%S)]: Splunk is already installed"
-else
-    echo "[$(date +%H:%M:%S)]: Installing Splunk..."
-# Get download.splunk.com into the DNS cache. Sometimes resolution randomly fails during wget below
-dig @8.8.8.8 download.splunk.com >/dev/null
-dig @8.8.8.8 splunk.com >/dev/null
-dig @8.8.8.8 www.splunk.com >/dev/null
+sudo yum install -y epel-release
 
-# Try to resolve the latest version of Splunk by parsing the HTML on the downloads page
-echo "[$(date +%H:%M:%S)]: Attempting to autoresolve the latest version of Splunk..."
-LATEST_SPLUNK=$(curl https://www.splunk.com/en_us/download/splunk-enterprise.html | grep -i deb | grep -Eo "data-link=\"................................................................................................................................" | cut -d '"' -f 2)
-# Sanity check what was returned from the auto-parse attempt
-if [[ "$(echo "$LATEST_SPLUNK" | grep -c "^https:")" -eq 1 ]] && [[ "$(echo "$LATEST_SPLUNK" | grep -c "\.deb$")" -eq 1 ]]; then
-echo "[$(date +%H:%M:%S)]: The URL to the latest Splunk version was automatically resolved as: $LATEST_SPLUNK"
-echo "[$(date +%H:%M:%S)]: Attempting to download..."
-wget --progress=bar:force -P /opt "$LATEST_SPLUNK"
-else
-echo "[$(date +%H:%M:%S)]: Unable to auto-resolve the latest Splunk version. Falling back to hardcoded URL..."
-# Download Hardcoded Splunk
-wget --progress=bar:force -O /opt/splunk-8.0.2-a7f645ddaf91-linux-2.6-amd64.deb 'https://download.splunk.com/products/splunk/releases/8.0.2/linux/splunk-8.0.2-a7f645ddaf91-linux-2.6-amd64.deb&wget=true'
-fi
-if ! ls /opt/splunk*.deb 1>/dev/null 2>&1; then
-echo "Something went wrong while trying to download Splunk. This script cannot continue. Exiting."
-exit 1
-fi
-if ! dpkg -i /opt/splunk*.deb >/dev/null; then
-echo "Something went wrong while trying to install Splunk. This script cannot continue. Exiting."
-exit 1
-fi
+#Setup Firewall Ports To Access Kibana/Elasticsearch
 
-#Setupsplunk
+sudo systemctl enable firewalld
 
-sudo /opt/splunk/bin/splunk enable boot-start
-sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd changeme
-sudo /opt/splunk/bin/splunk add index wineventlog -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk add index sysmon -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk add index powershell -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk add index zeek -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk add index suricata -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk add index threathunting -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk start
-sudo /opt/splunk/bin/splunk install app /remote/apps/splunk-add-on-for-microsoft-windows_800.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/splunk-add-on-for-microsoft-sysmon_1062.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/lookup-file-editor_346.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/splunk-add-on-for-zeek-aka-bro_400.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/force-directed-app-for-splunk_303.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/punchcard-custom-visualization_140.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/splunk-sankey-diagram-custom-visualization_150.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/link-analysis-app-for-splunk_163.tgz -auth 'admin:changeme'
-sudo /opt/splunk/bin/splunk install app /remote/apps/threathunting_144.tgz -auth 'admin:changeme'
-sudo sed -i 's/EVAL-host_fqdn = Computer/EVAL-host_fqdn = ComputerName/g' /opt/splunk/etc/apps/ThreatHunting/default/props.conf
-sudo /opt/splunk/bin/splunk restart
+sudo systemctl start firewalld
 
-#UpdateandUpgradeRepo
+sudo firewall-cmd --add-port=9200/tcp --permanent
 
-sudo apt-get update -y
+sudo firewall-cmd --add-port=5601/tcp --permanent
 
-sudo apt-get upgrade -y
+sudo firewall-cmd --reload
 
-sudo apt install apt-transport-https
+#Prepare to install Elasticsearch
 
-#InstallPowershell
+sudo yum -y install java-11-openjdk  java-11-openjdk-devel
 
-cd /home/vagrant
+sudo yum install -y nano
 
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+#Add Elastic GPG Key
 
-sudo dpkg -i packages-microsoft-prod.deb
+sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-sudo apt-get update -y
+#Create Elasticsearch repo file and add necesary contents 
 
-sudo apt-get install -y powershell
+sudo cat > /etc/yum.repos.d/elasticsearch.repo << EOF
+[elasticsearch]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=0
+autorefresh=1
+type=rpm-md
+EOF
 
-#CloneandDownloadAtomicRedTeam
+#Install Elasticsearch
 
-cd /home/vagrant/
+sudo yum install -y --enablerepo=elasticsearch elasticsearch
+
+#Configure Elasticsearch
+
+sudo sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml
+
+sudo sed -i 's/#http.port: 9200/http.port: 9200/' /etc/elasticsearch/elasticsearch.yml
+
+sudo sed -i 's/#discovery.seed_hosts: \["host1", "host2"\]/discovery.type: single-node/' /etc/elasticsearch/elasticsearch.yml
+
+sudo cat > /etc/default/elasticsearch <<EOF
+ES_PATH_CONF=/etc/elasticsearch
+ES_STARTUP_SLEEP_TIME=5
+MAX_OPEN_FILES=65536
+MAX_LOCKED_MEMORY=unlimited
+EOF
+
+sudo mkdir /etc/systemd/system/elasticsearch.service.d/
+sudo cat > /etc/systemd/system/elasticsearch.service.d/override.conf <<EOF
+[Service]
+LimitMEMLOCK=infinity
+EOF
+
+sudo cat > /etc/security/limits.conf <<EOF
+elasticsearch soft nofile 65536
+elasticsearch hard nofile 65536
+elasticsearch soft memlock unlimited
+elasticsearch hard memlock unlimited
+EOF
+
+#Ensure Elasticsearch is the owner of the /etc/elasticsearch directory
+
+sudo chown -R elasticsearch:elasticsearch /etc/elasticsearch 
+
+#Start Elasticsearch
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable elasticsearch.service
+
+sudo systemctl start elasticsearch.service
+
+sudo systemctl status elasticsearch.service
+
+#Create Kibana repo file and add necesary contents 
+
+sudo cat > /etc/yum.repos.d/kibana.repo << EOF
+[kibana-7.x]
+name=Kibana repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+
+#Install Kibana
+
+sudo yum install -y kibana
+
+#Configure Kibana
+
+sudo sed -i 's/#server.host: "localhost"/server.host: 0.0.0.0/' /etc/kibana/kibana.yml
+
+sudo sed -i 's/#server.port: 5601/server.port: 5601/' /etc/kibana/kibana.yml
+
+sudo sed -i 's/#elasticsearch.hosts: \["http:\/\/localhost:9200"\]/elasticsearch.hosts: "http:\/\/localhost:9200"/' /etc/kibana/kibana.yml
+
+#Ensure Kibana is the owner of the /etc/kibana directory
+
+sudo chown -R kibana:kibana /etc/kibana
+
+#Start Kibana
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable kibana.service
+
+sudo systemctl start kibana.service
+
+sudo systemctl status kibana.service
+
+#Install Powershell
+
+sudo curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+
+sudo yum install -y powershell
+
+#Clone, Download and Install AtomicRedTeam
+
+sudo yum install git -y
 
 sudo git clone https://github.com/redcanaryco/invoke-atomicredteam.git
 
-cd /home/vagrant/invoke-atomicredteam
+cd invoke-atomicredteam
 
-pwsh install-atomicredteam.ps1
+sudo pwsh install-atomicredteam.ps1
 
-pwsh install-atomicsfolder.ps1
+sudo pwsh install-atomicsfolder.ps1
 
-pwsh -command import-module /remote/binaries/invoke-atomicredteam/Invoke-AtomicRedTeam.psm1 -Force
+sudo pwsh -command import-module ./Invoke-AtomicRedTeam.psm1 -Force
+
+#Install Elasticsearch X-Pack
+
+
+
 
 
 
