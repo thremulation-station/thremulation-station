d4b54918f88b19ebb0c5123b4850901855a4daa8
Sean Cochran <sean.cochran@gmail.com>
2020-11-06 13:53:46 -0600

    add logic flow to beats setup scripts

--- vagrant/scripts/setup-linux-beats.sh
+++ vagrant/scripts/setup-linux-beats.sh
@@ -1,11 +1,45 @@
 #!/bin/bash -eux
 
-echo "Running Filebeat Setup"
-sleep 3
+set -euo pipefail
 
-sudo filebeat setup
+check_elastic() {
+  curl -sL \
+  -w "%{http_code}\\n" \
+  "http://192.168.33.10:9200/" \
+  -o /dev/null \
+  --connect-timeout 3 \
+  --max-time 5
+}
 
-echo "Running Auditbeat Setup"
-sleep 3
+echo "Waiting for Elasticsearch...";
+sleep 360;
 
-sudo auditbeat setup --dashboards
+if [ $(check_elastic) == "200" ] ; 
+then
+  echo "Elastic box is reachable -- proceeding with Beats setup:" ;
+  sleep 2;
+  echo "Running Filebeat Setup";
+  sudo filebeat setup;
+  echo "Running Auditbeat Setup";
+  sudo auditbeat setup --dashboards;
+  echo "Setup scripts complete!";
+else
+  echo "Unable to contact elastic over port 9200 -- Quitting." ;
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vagrant/scripts/setup-winlogbeat.ps1
+++ vagrant/scripts/setup-winlogbeat.ps1
@@ -1,6 +1,23 @@
+# First we create the request.
+$HTTP_Request = [System.Net.WebRequest]::Create('http://192.168.33.10:9200')
 
-echo "Running Winlogbeat Setup Command"
-Start-Sleep -s 3
+# We then get a response from the site.
+$HTTP_Response = $HTTP_Request.GetResponse()
 
-winlogbeat --path.config C:\ProgramData\chocolatey\lib\winlogbeat\tools setup
+# We then get the HTTP code as an integer.
+$HTTP_Status = [int]$HTTP_Response.StatusCode
 
+If ( $HTTP_Status -eq 200 ) {
+  Write-Host "Elastic box is reachable -- proceeding:" 
+  Start-Sleep -s 2
+  Write-Host "Running Winlogbeat Setup Command"
+  winlogbeat --path.config C:\ProgramData\chocolatey\lib\winlogbeat\tools setup
+}
+Else {
+  Write-Host "Unable to contact elastic over port 9200 -- Quitting."
+  Start-Sleep -s 2
+}
+
+# Finally, we clean up the http request by closing it.
+If ($HTTP_Response -eq $null) { } 
+Else { $HTTP_Response.Close() }
