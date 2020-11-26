1c4a931a278532babf47cab47cd850eac7318561
Kirk K <koelslaw@gmail.com>
2020-11-26 12:38:07 -0800

    Create setup.sh
    First stab at windows/*nix setup script

---
+++ vagrant/setup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+echo "Shall we play a game? -WOPR"
+while true; do
+    read -p "Do you want install the Windows 10 [Y/N]?" yn
+    case $yn in
+        [Yy]* ) WINDOWS="windows10"; break;;
+        [Nn]* ) WINDOWS=""; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+while true; do
+    read -p "Do you install Centos [Y/N]" yn
+    case $yn in
+        [Yy]* ) CENTOS="centos"; break;;
+        [Nn]* ) CENTOS=""; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+while true; do
+    read -p "Download (D) or Download and Execute (E) or Cancel (C)? [D\E\C]" yn
+    case $yn in
+        [Ee]* ) vagrant up elastic $CENTOS $WINDOWS; break;;
+        [Dd]* ) vagrant box update elastic $CENTOS $WINDOWS; break;;
+        [Cc]* ) break;;
+        * ) echo "Please answer yes or no or cancel.";;
+    esac
+done
+sleep 15
