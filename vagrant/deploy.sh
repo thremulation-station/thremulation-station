63bb9f354410c073fc2277dafab36b88706fe972
variable <peasead@gmail.com>
2020-12-03 20:52:41 -0600

    updated deploy script to work and include elastic

--- vagrant/deploy.sh
+++ vagrant/deploy.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash
 #set -eux
 
 banner()
@@ -9,36 +9,52 @@ banner()
     echo ""
 }
 
-
+# Install the Windows 10 host
 while true; do
     clear
     banner
-    read -p "Do you want deploy the Windows10 node [Y/N] ? " yn
+    read -p "Do you want deploy the Windows10 node (victim) [Y/N] ? " yn
     case $yn in
         [Yy]* ) WINDOWS="windows10"; break;;
         [Nn]* ) WINDOWS=""; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+# Install the Elastic Stack
 while true; do
     clear
     banner
-    read -p "Do you deploy the Centos7 node [Y/N] ? " yn
+    read -p "Do you want deploy the Elastic Stack [Y/N] ? " yn
+    case $yn in
+        [Yy]* ) ELASTIC="elastic"; break;;
+        [Nn]* ) ELASTIC=""; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Install Atomic Red
+while true; do
+    clear
+    banner
+    read -p "Do you deploy the Atomic Red node (attacker) [Y/N] ? " yn
     case $yn in
         [Yy]* ) CENTOS="centos"; break;;
         [Nn]* ) CENTOS=""; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+#
 while true; do
     clear
     banner
-    read -p "Do you want to (D)ownload / Download & (E)xecute / (C)ancel ? " yn
+    read -p "Do you want to just (D)ownload or Download & (I)nstall or (C)ancel ? " yn
     case $yn in
-        [Ee]* ) echo 'vagrant up elastic $CENTOS $WINDOWS'; break;;
-        [Dd]* ) echo 'vagrant box update elastic $CENTOS $WINDOWS'; break;;
+        [Ii]* ) vagrant up $ELASTIC $CENTOS $WINDOWS; break;;
+        [Dd]* ) vagrant box update $ELASTIC $CENTOS $WINDOWS; break;;
         [Cc]* ) break;;
-        * ) echo "Please answer yes or no or cancel.";;
+        * ) echo "Please answer (D)ownload, (I)nstall, or (C)ancel.";;
     esac
 done
 
