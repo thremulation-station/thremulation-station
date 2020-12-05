917bd7c91dcf39a111a43bb749bb2ea3bf37ddae
Derek Ditch <derek@rocknsm.io>
2020-12-05 16:24:07 -0600

    Fix some linting

--- vagrant/scripts/install-ea.ps1
+++ vagrant/scripts/install-ea.ps1
@@ -12,30 +12,33 @@
   }
   $bodyMsg = @{"forceRecreate" = "true"}
   $bodyJson = ConvertTo-Json($bodyMsg)
+  $kibana_url = "http://192.168.33.10:5601"
+  $elasticsearch_url = "http://192.168.33.10:9200"
 
   #RetrieveVersion
-  Invoke-WebRequest -UseBasicParsing http://192.168.33.10:9200 -OutFile version.json
+  Invoke-WebRequest -UseBasicParsing $elasticsearch_url -OutFile version.json
   $agent_version = (Get-Content 'version.json' | ConvertFrom-Json).version.number
 
   # Create Fleet User
   Write-Output "Create Fleet User"
-  Write-Output "Creating fleet user at http://192.168.33.10:5601/api/fleet/setup"
+  Write-Output "Creating fleet user at $kibana_url/api/fleet/setup"
   $fleetCounter = 0
   do {
   Start-Sleep -Seconds 20
   Write-Output "Trying $fleetCounter times"
   try{
-  Write-Output "Creating fleet user with POST request at http://192.168.33.10:5601/api/fleet/setup"
-  Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/agents/setup" -ContentType "application/json" -Headers $headers -Method POST -body $bodyJson -ErrorAction SilentlyContinue -ErrorVariable SearchError
+  Write-Output "Creating fleet user with POST request at $kibana_url/api/fleet/setup"
+  Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/agents/setup" -ContentType "application/json" -Headers $headers -Method POST -body $bodyJson -ErrorAction SilentlyContinue -ErrorVariable SearchError
   }
   catch{
   Write-output "Error Message Array: $searchError"
   }
   Start-Sleep -Seconds 5
+  
   # Checking the content output to see if the host is ready.
   try{
-  Write-Output "Checking if Fleet Manager is ready with GET request http://192.168.33.10:5601/api/fleet/enrollment-api-keys?page=1&perPage=20"
-  $ekIDBody = (Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/agent_policies?page=1&perPage=20&sortField=updated_at&sortOrder=desc&kuery=" -ContentType "application/json" -Headers $headers -Method GET  -ErrorVariable SearchError)
+  Write-Output "Checking if Fleet Manager is ready with GET request $kibana_url/api/fleet/enrollment-api-keys?page=1&perPage=20"
+  $ekIDBody = (Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/agent_policies?page=1&perPage=20&sortField=updated_at&sortOrder=desc&kuery=" -ContentType "application/json" -Headers $headers -Method GET  -ErrorVariable SearchError)
   $isReady = (convertfrom-json($ekIDBody.content)).total
   }
   catch{
@@ -48,18 +51,18 @@
 
   # Get Body of Fleet Enrollment API Key
   Write-Output "Get Enrollment API Key"
-  $ApiKeyList = (ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/enrollment-api-keys" -ContentType "application/json" -Headers $headers -Method GET))
+  $ApiKeyList = (ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/enrollment-api-keys" -ContentType "application/json" -Headers $headers -Method GET))
 
   # Get Fleet TOken from json message
   $ApiKeyId = $ApiKeyList.list[0].id
 
-  $ApiKeyActual = (ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/enrollment-api-keys/$ApiKeyId" -ContentType "application/json" -Headers $headers -Method GET))
+  $ApiKeyActual = (ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/enrollment-api-keys/$ApiKeyId" -ContentType "application/json" -Headers $headers -Method GET))
   $fleetToken = $ApiKeyActual.item[0].api_key
   $policyId = $ApiKeyActual.item[0].policy_id
 
   # Get list of current packages for an up to date Endpoint Version
-  $packageList = (convertfrom-json(Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/epm/packages" -ContentType "application/json" -Headers $headers -Method GET))
-  $endpointPackageVersion = ($packageList.response | where {$_.name -eq "endpoint"}).version
+  $packageList = (convertfrom-json(Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/epm/packages" -ContentType "application/json" -Headers $headers -Method GET))
+  $endpointPackageVersion = ($packageList.response | Where-Object {$_.name -eq "endpoint"}).version
 
   # Create a json request format suitable for the configuration id
   $securityConfigDict = @"
@@ -82,7 +85,7 @@
   $securityConfigDictJson = ConvertTo-Json($securityConfigDict)
 
   Write-Output "Enable Security Integration into Default Config in Ingest Manager"
-  Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/package_policies" -ContentType "application/json" -Headers $headers -Method POST -body $securityConfigDictJson
+  Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/package_policies" -ContentType "application/json" -Headers $headers -Method POST -body $securityConfigDictJson
 
   # Create a json request format suitable for the configuration id
   $windowsConfigDict = @"
@@ -105,33 +108,33 @@
   $windowsConfigDictJson = ConvertTo-Json($windowsConfigDict)
 
   Write-Output "Enable Windows Integration into Default Config in Ingest Manager"
-  Invoke-WebRequest -UseBasicParsing -Uri  "http://192.168.33.10:5601/api/fleet/package_policies" -ContentType "application/json" -Headers $headers -Method POST -body $windowsConfigDictJson
+  Invoke-WebRequest -UseBasicParsing -Uri  "$kibana_url/api/fleet/package_policies" -ContentType "application/json" -Headers $headers -Method POST -body $windowsConfigDictJson
 
   Write-Output "Set Kibana Url"
   $fleetYMLconfig = @"
   {
-  "kibana_urls": ["http://192.168.33.10:5601"]
+  "kibana_urls": ["$kibana_url"]
   }
 "@ | ConvertFrom-Json
 
   $fleetYMLconfigJson = ConvertTo-Json($fleetYMLconfig)
 
-  Invoke-WebRequest -UseBasicParsing -Uri "http://192.168.33.10:5601/api/fleet/settings" -ContentType application/json -Headers $headers -Method Put -body $fleetYMLconfigJson -ErrorAction SilentlyContinue -ErrorVariable SearchError -TransferEncoding compress
+  Invoke-WebRequest -UseBasicParsing -Uri "$kibana_url/api/fleet/settings" -ContentType application/json -Headers $headers -Method Put -body $fleetYMLconfigJson -ErrorAction SilentlyContinue -ErrorVariable SearchError -TransferEncoding compress
 
   Write-Output "Set Elasticsearch Url"
   $fleetYMLconfig2 = @"
   {
-  "hosts": ["http://192.168.33.10:9200"]
+  "hosts": ["$elasticsearch_url"]
   }
 "@ | ConvertFrom-Json
 
   $fleetYMLconfig2Json = ConvertTo-Json($fleetYMLconfig2)
 
-  $response = Invoke-RestMethod -UseBasicParsing -Uri "http://192.168.33.10:5601/api/fleet/outputs" -ContentType application/json -Headers $headers -Method Get -ErrorAction SilentlyContinue -ErrorVariable SearchError
+  $response = Invoke-RestMethod -UseBasicParsing -Uri "$kibana_url/api/fleet/outputs" -ContentType application/json -Headers $headers -Method Get -ErrorAction SilentlyContinue -ErrorVariable SearchError
 
   $id = $response.items.id
 
-  Invoke-WebRequest -UseBasicParsing -Uri "http://192.168.33.10:5601/api/fleet/outputs/$id" -ContentType application/json -Headers $headers -Method Put -body $fleetYMLconfig2Json -ErrorAction SilentlyContinue -ErrorVariable SearchError -TransferEncoding compress
+  Invoke-WebRequest -UseBasicParsing -Uri "$kibana_url/api/fleet/outputs/$id" -ContentType application/json -Headers $headers -Method Put -body $fleetYMLconfig2Json -ErrorAction SilentlyContinue -ErrorVariable SearchError -TransferEncoding compress
 
   $elasticAgentUrl = "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-$agent_version-windows-x86_64.zip"
   $agent_install_folder = "C:\Program Files"
@@ -149,14 +152,14 @@
 
   Rename-Item "$agent_install_folder\elastic-agent-$agent_version-windows-x86_64" "$agent_install_folder\Elastic-Agent"
 
-  Write-Output "Running enroll process of Elastic Agent with token: $fleetToken at url: http://192.168.33.10:5601"
+  Write-Output "Running enroll process of Elastic Agent with token: $fleetToken at url: $kibana_url"
   #install -f --kibana-url=KIBANA_URL --enrollment-token=ENROLLMENT_KEY
-  cd 'C:\Program Files\Elastic-Agent'
-  .\elastic-agent.exe install -f --insecure --kibana-url=http://192.168.33.10:5601 --enrollment-token=$fleetToken
+  Set-Location 'C:\Program Files\Elastic-Agent'
+  .\elastic-agent.exe install -f --insecure --kibana-url=$kibana_url --enrollment-token=$fleetToken
 
   #Write-Output "Running Agent Install Process"
   # & "$agent_install_folder\elastic-agent-$agent_version-windows-x86_64\install-service-elastic-agent.ps1" -Wait
 
- cd 'C:\Program Files\Elastic\Agent\'
+ Set-Location 'C:\Program Files\Elastic\Agent\'
 
  .\elastic-agent.exe restart
