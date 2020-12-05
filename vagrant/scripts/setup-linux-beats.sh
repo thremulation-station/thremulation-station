f14a162e1363f3eadb6f40d999e53077860758ed
variable <peasead@gmail.com>
2020-12-05 11:34:18 -0600

    updated beats install for linux & win, added agent

--- vagrant/scripts/setup-linux-beats.sh
+++ vagrant/scripts/setup-linux-beats.sh
@@ -1,45 +1,18 @@
 #!/bin/bash
 
-set -euxo pipefail
-
-check_elastic() {
-  curl -kI --silent \
-  -w "%{http_code}\\n" \
-  "http://192.168.33.10:9200/" \
-  -o /dev/null \
-  --connect-timeout 3 \
-  --max-time 5
-}
-
-echo "Waiting for services to come up...";
-sleep 60;
-echo "Checking if Elasticsearch is available";
-
-if [ $(check_elastic) == "200" ] ;
-then
-  echo "Elastic box is reachable -- proceeding with Beats setup:" ;
-  sudo filebeat setup;
-  sudo auditbeat setup --dashboards;
-  echo "Setup scripts complete!";
-else
-  echo "Unable to contact elastic over port 9200 -- Quitting." ;
-fi
-
-exit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Wait for Elasticsearch to become available
+echo "Elasticsearch takes a bit to come up. Give it a minute."
+while true
+do
+  STATUS=$(curl -I http://192.168.33.10:9200 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+  if [ "${STATUS}" == "200" ]; then
+    echo "Elasticsearch is up. Proceeding"
+    sudo filebeat setup;
+    sudo auditbeat setup --dashboards;
+    echo "Setup script complete!";
+    break
+  else
+    echo "Elasticsearch still loading. Trying again in 10 seconds"
+  fi
+  sleep 10
+done
