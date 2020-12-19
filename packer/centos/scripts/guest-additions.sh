2bfe3831f48668ace94fc08f34662d7143f8b4cb
seven62 <sean.cochran@gmail.com>
2020-12-19 15:28:00 -0600

    troubleshooting guestadditions

--- packer/centos/scripts/guest-additions.sh
+++ packer/centos/scripts/guest-additions.sh
@@ -2,16 +2,45 @@
 
 curl --url http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso --output ./VBoxGuestAdditions_4.3.8.iso
 
-mkdir /media/VBoxGuestAdditions; error
+mkdir /media/VBoxGuestAdditions
 
-mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions; error
+mount -o loop,ro /home/vagrant/VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
 
-sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run; error
+sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
 
-rm VBoxGuestAdditions_4.3.8.iso; error
+umount /media/VBoxGuestAdditions
 
-umount /media/VBoxGuestAdditions; error
+rm /home/vagrant/VBoxGuestAdditions_4.3.8.iso
 
-rmdir /media/VBoxGuestAdditions; error
+rmdir /media/VBoxGuestAdditions
 
 echo "----- Guest Additions Successfully Installed -----"
+
+
+
+
+# # Mount the disk image
+# cd /tmp
+# mkdir /tmp/isomount
+# mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /tmp/isomount
+
+# # Install the drivers
+# /tmp/isomount/VBoxLinuxAdditions.run
+
+# # Cleanup
+# umount isomount
+# rm -rf isomount /root/VBoxGuestAdditions.iso
+
+
+#   # Guest additions are located as per guest_additions_path in 
+#   # Packer's configuration file
+#   - name: Mount guest additions ISO read-only
+#     mount:
+#       path: /mnt/
+#       src: /home/vagrant/VBoxGuestAdditions.iso
+#       fstype: iso9660
+#       opts: ro
+#       state: mounted
+
+#   - name: execute guest additions
+#     shell: /mnt/VBoxLinuxAdditions.run 
