537f3c74cb8d2f081ac044e0d646b4dccb04f69f
webhead404 <kvvincentvalentine@gmail.com>
2021-08-25 11:50:43 -0400

    Dev v0.9.3 fleet changes (#81)
    * replace discord with slack
    
    * All the changes for dev-v0.9.3
    
    Updated agents scripts, new fleet server script and Vagrantfile changes
    
    * Update Vagrantfile
    
    Co-authored-by: Sean Cochran <sean.cochran@gmail.com>
    Co-authored-by: DefSecSentinel <48036388+DefSecSentinel@users.noreply.github.com>

--- vagrant/scripts/install-ea-linux.sh
+++ vagrant/scripts/install-ea-linux.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-STACK_VER="${ELASTIC_STACK_VERSION:-7.10.1}"
+STACK_VER="${ELASTIC_STACK_VERSION:-7.14.0}"
 KIBANA_URL="${KIBANA_URL:-http://127.0.0.1:5601}"
 KIBANA_AUTH="${KIBANA_AUTH:-}"
 
@@ -19,7 +19,9 @@ function download_and_install_agent() {
     cd "$(mktemp -d)"
     curl --silent -LJ "${AGENT_URL}" | tar xzf -
     cd "$(basename "$(basename "${AGENT_URL}")" .tar.gz)"
-    sudo ./elastic-agent install --force --insecure --kibana-url="${KIBANA_URL}" --enrollment-token="${ENROLLMENT_TOKEN}"
+    #sudo ./elastic-agent install --force --insecure --kibana-url="${KIBANA_URL}" --enrollment-token="${ENROLLMENT_TOKEN}"
+
+    sudo ./elastic-agent install --force --insecure --url="${FLEET_SERVER_URL}" --enrollment-token="${ENROLLMENT_TOKEN}"
 
     # Cleanup temporary directory
     cd ..
@@ -30,18 +32,23 @@ function download_and_install_agent() {
 function get_enrollment_token() {
     declare -a AUTH=()
     declare -a HEADERS=(
-        "-H" "Content-Type: application/json"
+        "-H" "Content-Type: application/json",
+        "-H" "kbn-xsrf: 7.14.0"
     )
 
     if [ -n "${KIBANA_AUTH}" ]; then
         AUTH=("-u" "${KIBANA_AUTH}")
     fi
 
-    response=$(curl --silent "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/enrollment-api-keys")
-    enrollment_key_id=$(echo -n "${response}" | jq -r '.list[] | select(.name | startswith("Default")) | .id' )
-    enrollment_key=$(curl --silent "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/enrollment-api-keys/${enrollment_key_id}" | jq -r '.item.api_key')
+    POLICY_ID=$(curl --silent -XGET "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/agent_policies" | jq --raw-output '.items[] | select(.name | startswith("Default policy")) | .id')
 
-    echo -n "${enrollment_key}"
+    ENROLLMENT_TOKEN=$(curl --silent -XGET "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/enrollment-api-keys" | jq --arg POLICY_ID "$POLICY_ID" -r '.list[] | select(.policy_id==$POLICY_ID) | .api_key')
+
+    #response=$(curl --silent "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/enrollment-api-keys")
+    #enrollment_key_id=$(echo -n "${response}" | jq -r '.list[] | select(.name | startswith("Default")) | .id' )
+    #enrollment_key=$(curl --silent "${AUTH[@]}" "${HEADERS[@]}" "${KIBANA_URL}/api/fleet/enrollment-api-keys/${enrollment_key_id}" | jq -r '.item.api_key')
+
+    echo -n "${ENROLLMENT_TOKEN}"
 }
 
 install_jq
