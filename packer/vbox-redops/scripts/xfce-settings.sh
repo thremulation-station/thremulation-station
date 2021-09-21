fe09959349c7b5635dca9f822fdf163a817f684d
webhead404 <kvvincentvalentine@gmail.com>
2021-09-21 15:40:07 -0500

    Create xfce-settings.sh

---
+++ packer/vbox-redops/scripts/xfce-settings.sh
@@ -0,0 +1,16 @@
+# Add these settings for staging when we get to Vagrant
+
+VAGRANT_HOME="/home/vagrant"
+
+touch $VAGRANT_HOME/.config/user-dirs.dirs
+
+echo "
+XDG_DESKTOP_DIR="$VAGRANT_HOME/Desktop"
+XDG_DOWNLOAD_DIR="$VAGRANT_HOME/Downloads"
+XDG_TEMPLATES_DIR="$VAGRANT_HOME/Templates"
+XDG_PUBLICSHARE_DIR="$VAGRANT_HOME/Public"
+XDG_DOCUMENTS_DIR="$VAGRANT_HOME/Documents"
+XDG_MUSIC_DIR="$VAGRANT_HOME/Music"
+XDG_PICTURES_DIR="$VAGRANT_HOME/Pictures"
+XDG_VIDEOS_DIR="$VAGRANT_HOME/Videos"
+" > $VAGRANT_HOME/.config/user-dirs.dirs
