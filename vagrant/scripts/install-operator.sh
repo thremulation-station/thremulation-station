f7361a85fc5975db4d23e073b427a74497651594
webhead404 <kvvincentvalentine@gmail.com>
2021-09-21 11:13:44 -0500

    Moved operator location

--- vagrant/scripts/install-operator.sh
+++ vagrant/scripts/install-operator.sh
@@ -7,9 +7,9 @@ echo "Installing Operator"
 
 cd "$(mktemp -d)"
 curl --silent -LJ $OPERATOR_URL -o operator.appImage
-cp operator.appImage $VAGRANT_HOME
-cd $VAGRANT_HOME
-
+cp operator.appImage $VAGRANT_HOME/Desktop
+cd $VAGRANT_HOME/Desktop
+mv operator.appImage Operator
 chmod +x operator.appImage && chown vagrant: operator.appImage
 
 # For when Operator gets support for Debian directly
