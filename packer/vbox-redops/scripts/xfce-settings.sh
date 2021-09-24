c372466536794a09f183afce7e40116f075e4d4b
webhead404 <kvvincentvalentine@gmail.com>
2021-09-23 20:48:07 -0500

    Update xfce-settings.sh

--- packer/vbox-redops/scripts/xfce-settings.sh
+++ packer/vbox-redops/scripts/xfce-settings.sh
@@ -2,18 +2,6 @@
 
 VAGRANT_HOME="/home/vagrant"
 
-mkdir $VAGRANT_HOME/{Desktop,Downloads,Templates,Public,Documents,Music,Pictures,Videos}
-chown -R $VAGRANT_HOME/*
-
-touch $VAGRANT_HOME/.config/user-dirs.dirs
-
-echo "
-XDG_DESKTOP_DIR="$VAGRANT_HOME/Desktop"
-XDG_DOWNLOAD_DIR="$VAGRANT_HOME/Downloads"
-XDG_TEMPLATES_DIR="$VAGRANT_HOME/Templates"
-XDG_PUBLICSHARE_DIR="$VAGRANT_HOME/Public"
-XDG_DOCUMENTS_DIR="$VAGRANT_HOME/Documents"
-XDG_MUSIC_DIR="$VAGRANT_HOME/Music"
-XDG_PICTURES_DIR="$VAGRANT_HOME/Pictures"
-XDG_VIDEOS_DIR="$VAGRANT_HOME/Videos"
-" > $VAGRANT_HOME/.config/user-dirs.dirs
+touch $VAGRANT_HOME/.xsession
+chown vagrant: $VAGRANT_HOME/.xsession && chmod +x $VAGRANT_HOME/.xsession
+echo "xfce4-session" > $VAGRANT_HOME/.xsession
