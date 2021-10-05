67b4e07d72b9e6458b8d80072c53111014c3ee50
webhead404 <kvvincentvalentine@gmail.com>
2021-10-05 16:07:22 -0500

    Bootstrapping Operator (#108)
    Co-authored-by: Sean Cochran <sean.cochran@gmail.com>
    
    fixes #107

--- vagrant/scripts/install-operator.sh
+++ vagrant/scripts/install-operator.sh
@@ -14,10 +14,26 @@ chmod +x Operator.appImage && chown vagrant: Operator.appImage
 
 
 # Dropping bootstrap script
-#echo "Setting up first boot for Operator"
-#mkdir /opt/operator
-#cp /vagrant/bootstrap_operator.sh /opt/operator
 
+echo "Setting up first boot for Operator"
+#mkdir /opt/operator
+#chown vagrant: /opt/operator
+cp /vagrant/scripts/bootstrap_operator.sh $VAGRANT_HOME/Desktop
+chown vagrant: $VAGRANT_HOME/Desktop/bootstrap_operator.sh
+
+echo "[Unit]
+Description=Bootstrap-Operator
+[Service]
+ExecStart=/opt/operator/bootstrap_operator.sh
+Restart=on-failure
+StartLimitInterval=600
+RestartSec=15
+StartLimitBurst=16
+[Install]
+WantedBy=multi-user.target" > bootstrap-operator.service
+cp bootstrap-operator.service /etc/systemd/system
+
+bash /opt/operator/bootstrap_operator.sh
 
 # For when Operator gets support for Debian directly
 #dpkg -i operator.deb
