d8e0adf111af63e887e7db9cd483344458f8203c
webhead404 <kvvincentvalentine@gmail.com>
2021-10-11 23:00:48 -0500

    Made bootstrap_operator executable

--- vagrant/scripts/install-operator.sh
+++ vagrant/scripts/install-operator.sh
@@ -20,20 +20,23 @@ echo "Setting up first boot for Operator"
 #chown vagrant: /opt/operator
 cp /vagrant/scripts/bootstrap_operator.sh $VAGRANT_HOME/Desktop
 chown vagrant: $VAGRANT_HOME/Desktop/bootstrap_operator.sh
+chmod +x $VAGRANT_HOME/Desktop/bootstrap_operator.sh
 
-echo "[Unit]
-Description=Bootstrap-Operator
-[Service]
-ExecStart=/opt/operator/bootstrap_operator.sh
-Restart=on-failure
-StartLimitInterval=600
-RestartSec=15
-StartLimitBurst=16
-[Install]
-WantedBy=multi-user.target" > bootstrap-operator.service
-cp bootstrap-operator.service /etc/systemd/system
 
-bash /opt/operator/bootstrap_operator.sh
+## Idea for a service. Needs some love and attention. 
+#echo "[Unit]
+#Description=Bootstrap-Operator
+#[Service]
+#ExecStart=/opt/operator/bootstrap_operator.sh
+#Restart=on-failure
+#StartLimitInterval=600
+#RestartSec=15
+#StartLimitBurst=16
+#[Install]
+#WantedBy=multi-user.target" > bootstrap-operator.service
+#cp bootstrap-operator.service /etc/systemd/system
+
+#bash /opt/operator/bootstrap_operator.sh
 
 # For when Operator gets support for Debian directly
 #dpkg -i operator.deb
