4c8ccbad8fc89968c6ba753ba3cbcbc40c01581e
webhead404 <kvvincentvalentine@gmail.com>
2022-02-24 10:21:44 -0600

    Update start-pneuma.ps1

--- vagrant/scripts/start-pneuma.ps1
+++ vagrant/scripts/start-pneuma.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "C:\Pneuma\pneuma-windows.exe" -ArgumentList "-address 192.168.56.13:2323 -contact tcp -name pneuma-window -range thremulation"
+Start-Process -FilePath "C:\Pneuma\pneuma-windows.exe" -ArgumentList "-address 192.168.56.13:2323 -contact tcp -name pneuma-windows10 -range thremulation"
